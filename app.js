332ff520ad9218e38c7eefe25de96ce95db9d94f
basilKeldhose <beldhose2015@gmail.com>
2020-11-06 14:12:05 +0530

    second commit

--- app.js
+++ app.js
@@ -3,37 +3,37 @@ const app = express();
 const fs = require('fs')
 const path = require('path')
 const bodyParser = require("body-parser")
+
 const mongoose = require('mongoose')
 require('./dbSeller')
 require('./dbproduct')
 const Sellers = mongoose.model('sellers')
 const Products = mongoose.model('products')
+
 const { check, validationResult } = require("express-validator");
 const { json } = require('body-parser');
 const stripe = require('stripe')('sk_test_51Hh7GSDCUMC8SpWDMjJOEKTcIfwfLVlKQxGaPqvuMiMxF1vwFhrTu6tJBtyOERwUfq5Ks9uckfKDgNSw4ZATEp1A00bJ7K2VaH');
-
-
-const connection_Url = 'mongodb+srv://admin:XXpJ0kIxku72aaZd@cluster0.vtn35.mongodb.net/stripe-api?retryWrites=true&w=majority'
-
-app.use(express.json());
-app.use(express.static('public'))
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-
-app.get('/sellers', (req, res) => {
-    res.render('seller')
-})
-
+const stripePublicKey ="pk_test_51Hh7GSDCUMC8SpWD4K3SgUdlvMaLjSgyI3v4gmjZl5krco2Eph4NKuJvR1qJKkvFK1za7I9kOJHvdmqmkuhhDjF400o1hTiWTL"
 // DB CONFIG
+const connection_Url = 'mongodb+srv://admin:XXpJ0kIxku72aaZd@cluster0.vtn35.mongodb.net/stripe-api?retryWrites=true&w=majority'
 
 mongoose.connect(connection_Url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
-})
+});
 
+app.use(express.json());
+app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.get('/seller', (req, res) => {
+    res.render('seller')
+})
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 // API CONFIG
 app.post('/seller', urlencodedParser, [
     check('name', 'this name must be 3+ charecter')
@@ -42,66 +42,63 @@ app.post('/seller', urlencodedParser, [
     check('email', 'Email is not valied')
         .isEmail()
         .normalizeEmail()
-    ], (req, res) => {
-        const error = validationResult(req)
-        if(error.isEmpty()){
-            const alert =error.array()
-            res.render('seller',{
-                alert
-            })
+], (req, res) => {
+    const error = validationResult(req)
+    if (error.isEmpty()) {
+        const alert = error.array()
+        res.render('seller', {
+            alert
+        })
+    }
+    const seller = new Sellers()
+    seller.name = req.body.name
+    seller.email = req.body.email
+    seller.description = req.body.description
+    Sellers.create(seller, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
         }
-            const seller = new Sellers()
-            seller.name = req.body.name
-            seller.email = req.body.email
-            seller.description = req.body.description
-            Sellers.create(seller, (err, data) => {
+        else {
+            res.status(201).send(data);
+            var params = req.body
+            stripe.customers.create(params, (err, customer) => {
                 if (err) {
-                    res.status(500).send(err)
+                    console.log(err)
+                }
+                if (customer) {
+                    console.log("success" + customer)
                 }
                 else {
-                    res.status(201).send(data);
-                    var params = req.body
-                    stripe.customers.create(params, (err, customer) => {
-                        if (err) {
-                            console.log(err)
-                        }
-                        if (customer) {
-                            console.log("success" + customer)
-                        }
-                        else {
-                            console.log("something wrong")
-                        }
-                    })
+                    console.log("something wrong")
                 }
             })
-        });
-
+        }
+    })
+});
 
 app.post('/product', (req, res) => {
     const product = new Products()
     product.name = req.body.name
     product.price = req.body.price
     product.image_URL = req.body.image_URL
-    Products.create(product, (err,data) => {
+    Products.create(product, (err, data) => {
         if (err) {
             res.status(500).send(err)
         }
         else {
             res.status(201).send(data)
-            
-                }
-            })
-        
+
+        }
+    })
 });
 app.get("/product", (req, res) => {
     Products.find((err, data) => {
         if (err) {
             res.status(500).send(err);
-
         }
         else {
             res.status(200).send(data);
-            fs.writeFile('products.json', data, (err) => {
+            fs.appendFile('items.json',`[${data}]`, (err) => {
                 if (err) {
                     console.log(err)
                 }
@@ -112,8 +109,6 @@ app.get("/product", (req, res) => {
         }
     })
 })
-
-// STRIPE API 
 
 app.get('/store', (req, res) => {
     fs.readFile('items.json', (error, data) => {
@@ -155,7 +150,8 @@ app.post('/purchase', (req, res) => {
             })
         }
     })
- })
+})
+
 app.listen(8001, () => {
     console.log('server is connected on port in 8001..!!!');
 });
