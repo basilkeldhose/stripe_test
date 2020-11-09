93e85a0eefb3f96cd487c019779ae3a8d81249d0
basilKeldhose <beldhose2015@gmail.com>
2020-11-09 11:15:54 +0530

    sixth commit

--- dbSeller.js
+++ dbSeller.js
@@ -1,10 +1,7 @@
-const mongoose =require('mongoose')
-
+const mongoose =require('mongoose');
 const sellerschema =new mongoose.Schema({
     name:String,
     email:String,
     description:String
- 
-})
-
+});
 mongoose.model('sellers',sellerschema);
--- dbproduct.js
+++ dbproduct.js
@@ -1,8 +1,7 @@
-const mongoose =require("mongoose")
-
+const mongoose =require("mongoose");
 const productSchema= new mongoose.Schema({
     name:String,
     price:Number,
     image_URL:String
-})
+});
 mongoose.model('products', productSchema)
